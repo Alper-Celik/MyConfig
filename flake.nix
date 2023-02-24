c85d2ed5d326d095adee6617f7c37d0589bcfbaf
Alper Çelik <dev.alpercelik@gmail.com>
2023-02-24 23:15:33 +0300

    remove some comments

--- flake.nix
+++ flake.nix
@@ -119,20 +119,11 @@
 
       nixOnDroidConfigurations.default = nix-on-droid.lib.nixOnDroidConfiguration {
         extraSpecialArgs = specialArgs.aarch64-linux // { isFlake = true; };
-        # _module.args = {
-        #   inherit specialArgs;
-        # } // specialArgs;
         modules = [
           ./nix-on-droid/nix-on-droid.nix
         ];
         pkgs = legacyPackages.nixpkgs.aarch64-linux;
 
-        # modules = [
-        #   (input: {
-        #
-        #     system.stateVersion = "22.05";
-        #   })
-        # ];
       };
     };
 }
