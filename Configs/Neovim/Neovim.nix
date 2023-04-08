1133227afd33ffcb0234b02e460d58036f4bd182
Alper Çelik <dev.alpercelik@gmail.com>
2023-04-08 15:57:10 +0300

    use neovim packages instead of home packages

--- Configs/Neovim/Neovim.nix
+++ Configs/Neovim/Neovim.nix
@@ -10,27 +10,27 @@
     package = pkgs.neovim-unwrapped;
     plugins = with pkgs.vimPlugins ;[
     ] ++ builtins.attrValues pkgs.otherNeovimPlugins;
+    extraPackages = with pkgs;
+      [
+        ## lsp s
+
+        rnix-lsp
+        sumneko-lua-language-server
+        cmake-language-server
+        nil
+        nodePackages.pyright
+        clang-tools # also formatter and static analysis
+        nodePackages.bash-language-server
+        taplo
+
+        ## formatters
+        nixpkgs-fmt
+        stylua
+        python310Packages.autopep8
+        cmake-format
+        nodePackages.prettier
+
+      ];
   };
-  home.packages = with pkgs;
-    [
-      ## lsp s
-
-      rnix-lsp
-      sumneko-lua-language-server
-      cmake-language-server
-      nil
-      nodePackages.pyright
-      clang-tools # also formatter and static analysis
-      nodePackages.bash-language-server
-      taplo
-
-      ## formatters
-      nixpkgs-fmt
-      stylua
-      python310Packages.autopep8
-      cmake-format
-      nodePackages.prettier
-
-    ];
   xdg.configFile.nvim.source = ./.;
 }
