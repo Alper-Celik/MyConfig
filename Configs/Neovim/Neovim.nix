a51a0c8c87a3cf1367da4273434b12f5bdb8d1f4
Alper Çelik <dev.alpercelik@gmail.com>
2023-05-03 19:26:06 +0300

    add language tools to enviroment too
    Signed-off-by: Alper Çelik <dev.alpercelik@gmail.com>

--- Configs/Neovim/Neovim.nix
+++ Configs/Neovim/Neovim.nix
@@ -1,4 +1,31 @@
 { inputs, lib, config, pkgs, specialArgs, ... }:
+let
+  language-tools = with pkgs;
+    [
+      ## lsp s
+
+      rnix-lsp
+      sumneko-lua-language-server
+      cmake-language-server
+      nil
+      nodePackages.pyright
+      clang-tools # also formatter and static analysis
+      nodePackages.bash-language-server
+      taplo
+      haskell-language-server
+      haskellPackages.fast-tags
+      haskellPackages.hoogle
+
+      ## formatters
+      nodePackages.cspell
+      nixpkgs-fmt
+      stylua
+      python310Packages.autopep8
+      cmake-format
+      nodePackages.prettier
+
+    ];
+in
 {
   programs.neovim = {
     enable = true;
@@ -10,31 +37,8 @@
     package = pkgs.neovim-unwrapped;
     plugins = with pkgs.vimPlugins ;[
     ] ++ builtins.attrValues pkgs.otherNeovimPlugins;
-    extraPackages = with pkgs;
-      [
-        ## lsp s
-
-        rnix-lsp
-        sumneko-lua-language-server
-        cmake-language-server
-        nil
-        nodePackages.pyright
-        clang-tools # also formatter and static analysis
-        nodePackages.bash-language-server
-        taplo
-        haskell-language-server
-        haskellPackages.fast-tags
-        haskellPackages.hoogle
-
-        ## formatters
-        nodePackages.cspell
-        nixpkgs-fmt
-        stylua
-        python310Packages.autopep8
-        cmake-format
-        nodePackages.prettier
-
-      ];
+    extraPackages = language-tools;
   };
   xdg.configFile.nvim.source = ./.;
+  home.packages = language-tools;
 }
