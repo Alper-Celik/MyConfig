d1d9946c26dadab7d0a559f80dc11a4ca12f2d5b
Alper Çelik <dev.alpercelik@gmail.com>
2023-05-03 15:47:23 +0300

    use floating toggleterm

--- Configs/Neovim/lua/my/keybindings/plugin_bindings.lua
+++ Configs/Neovim/lua/my/keybindings/plugin_bindings.lua
@@ -19,13 +19,12 @@ require("nvim-tree").setup({
 
 --terminal
 require("toggleterm").setup({
-	size = 20,
 	open_mapping = "<A-t>",
 	start_in_insert = true,
 	insert_mappings = true, -- whether or not the open mapping applies in insert mode
 	terminal_mappings = true, -- whether or not the open mapping applies in the opened terminals
 	shell = vim.o.shell,
-	direction = "horizontal",
+	direction = "float",
 })
 
 --#region telescope
