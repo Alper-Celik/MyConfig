0a8d92d3089adc940ea4b3d66fb829fba09a9cd9
Alper Çelik <dev.alpercelik@gmail.com>
2023-05-14 23:42:49 +0300

    use unstable (lastest) nix version

--- Configs/Nix-os.nix
+++ Configs/Nix-os.nix
@@ -1,6 +1,8 @@
-{ inputs, config, lib, specialArgs, ... }:
+{ inputs, pkgs, config, lib, specialArgs, ... }:
 {
   nix = {
+    package = pkgs.nixVersions.unstable;
+
     # This will add each flake input as a registry
     # To make nix3 commands consistent with your flake
     registry = lib.mapAttrs (_: value: { flake = value; }) inputs;
