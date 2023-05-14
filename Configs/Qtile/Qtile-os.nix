4249e3d30605953357f9e5aabde479669d0def32
Alper Çelik <dev.alpercelik@gmail.com>
2023-05-14 23:46:48 +0300

    initial qtile config

--- Configs/Qtile/Qtile-os.nix
+++ Configs/Qtile/Qtile-os.nix
@@ -1,10 +1,26 @@
 { ... }:
 {
-  services.xserver.windowManager.qtile = {
+
+  programs.light.enable = true;
+  users.users.alper.extraGroups = [ "video" ];
+
+  services.picom = {
     enable = true;
-    extraPackages = python3Packages: with python3Packages; [
-      qtile-extras
-    ];
-    backend = "x11";
+    vSync = true;
+    backend = "glx";
+  };
+
+  services.xserver = {
+    libinput = {
+      enable = true;
+      mouse.accelProfile = "flat";
+    };
+    windowManager.qtile = {
+      enable = true;
+      extraPackages = python3Packages: with python3Packages; [
+        qtile-extras
+      ];
+      backend = "x11";
+    };
   };
 }
