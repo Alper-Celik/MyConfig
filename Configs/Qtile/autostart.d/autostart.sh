44583370a6d82c967711d32adf9f5484ee061933
Alper Çelik <dev.alpercelik@gmail.com>
2023-05-25 21:18:55 +0300

    remove qtile

--- Configs/Qtile/autostart.d/autostart.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# keybindings
-setxkbmap -option caps:escape
-
-# screen
-case $HOSTNAME in
-  (lenovo-ideapad-510) xrandr --output eDP-1 --off --output HDMI-1 --off --output DP-1 --primary --mode 1440x900 --pos 1920x0 --rotate normal --output HDMI-2 --off # TODO: handle unplugged
-esac
