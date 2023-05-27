dcbaf9746dbebcfe7d81037f66170168ec084305
Alper Çelik <dev.alpercelik@gmail.com>
2023-05-28 01:02:16 +0300

    remove not needed bindings

--- Configs/Neovim/lua/my/keybindings/plugin_bindings.lua
+++ Configs/Neovim/lua/my/keybindings/plugin_bindings.lua
@@ -3,18 +3,6 @@ require("nvim-tree").setup({
 	git = {
 		ignore = false,
 	},
-	view = {
-		mappings = {
-			custom_only = false,
-			list = {
-				{ key = "<C-E>", action = "close" },
-				{ key = "l", action = "edit", action_cb = edit_or_open },
-				{ key = "L", action = "vsplit_preview", action_cb = vsplit_preview },
-				{ key = "h", action = "close_node" },
-				{ key = "H", action = "collapse_all", action_cb = collapse_all },
-			},
-		},
-	},
 })
 
 --terminal
