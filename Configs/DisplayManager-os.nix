6da64db50be50eef2abceb853867ca015de27e34
Alper Çelik <dev.alpercelik@gmail.com>
2023-07-08 14:30:04 +0300

    use wayland bby default

--- Configs/DisplayManager-os.nix
+++ Configs/DisplayManager-os.nix
@@ -1,11 +1,6 @@
 { config, lib, pkgs, ... }: {
   # default wayland
-  # services.xserver.displayManager.defaultSession = "plasmawayland";
-  # services.xserver.displayManager.sddm.enable = true;
-  # services.greetd = {
-  #   enable = true;
-  #   settings.default_session.command = "${pkgs.dbus}/bin/dbus-run-session env XKB_DEFAULT_LAYOUT=tr ${lib.getExe pkgs.cage} -s -mlast -- ${lib.getExe pkgs.st} -- ${lib.getExe pkgs.greetd.tuigreet} --remember --remember-session --asterisks --time -s $XDG_DATA_DIRS";
-  # };
+  services.xserver.displayManager.defaultSession = "plasmawayland";
 
   services.xserver.displayManager.gdm.wayland = true;
   services.xserver.displayManager.gdm.enable = true;
