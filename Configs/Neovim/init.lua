25ff46cd797b4e7bd1ae02f673a66ed44afff2f9
Alper Çelik <dev.alpercelik@gmail.com>
2023-07-23 19:23:06 +0300

    partialy switch to lazy nvim package manager

--- Configs/Neovim/init.lua
+++ Configs/Neovim/init.lua
@@ -1,3 +1,18 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
+end
+vim.opt.rtp:prepend(lazypath)
+
+vim.cmd([[packloadall]])
+
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 -- pcall(function()
--- Configs/Neovim/lua/my/plugins/init.lua
+++ Configs/Neovim/lua/my/plugins/init.lua
@@ -1,6 +1,73 @@
-require("lazy").setup({})
+require("lazy").setup({
+	-- Utulities
+	--# for reload functionality and some plugins
+	"nvim-lua/plenary.nvim",
+	--#comment
+	"numToStr/Comment.nvim",
+	--#error and todo window
+	"folke/trouble.nvim",
+	"folke/todo-comments.nvim",
+	--#icons
+	"nvim-tree/nvim-web-devicons",
+	--# fuzzy search
+	"nvim-telescope/telescope.nvim",
+	-- telescope-fzf-native-nvim = {
+	"nvim-telescope/telescope-fzf-native.nvim",
+	--   flake = false,
+	-- }, # add from nixpkgs
+
+	--# file explorer
+	"nvim-tree/nvim-tree.lua",
+	--# multi cursor
+	"mg979/vim-visual-multi",
+	--# rainbow
+	"https://gitlab.com/HiPhish/nvim-ts-rainbow2",
+
+	--flat sesion (like in git commit)
+	"willothy/flatten.nvim",
+
+	-- ui costumization
+	"EdenEast/nightfox.nvim",
+	"nvim-lualine/lualine.nvim",
+	"arkav/lualine-lsp-progress",
+
+	"stevearc/dressing.nvim",
+
+	"folke/noice.nvim",
+	"MunifTanjim/nui.nvim",
+	"rcarriga/nvim-notify",
+
+	-- debugging support
+	"mfussenegger/nvim-dap",
+
+	-- language support
+	"neovim/nvim-lspconfig",
+	"jose-elias-alvarez/null-ls.nvim",
+	"https://git.sr.ht/~p00f/clangd_extensions.nvim",
+	"simrat39/rust-tools.nvim",
+	{ "mrcjkb/haskell-tools.nvim", branch = "1.x.x" },
+
+	"Vigemus/iron.nvim",
+
+	"akinsho/toggleterm.nvim",
+
+	-- completion
+	"hrsh7th/nvim-cmp",
+	"hrsh7th/cmp-nvim-lsp",
+	"saadparwaiz1/cmp_luasnip",
+	"L3MON4D3/LuaSnip",
+	"windwp/nvim-autopairs",
+
+	-- nvim in browser
+	"glacambre/firenvim",
+}, {
+	performance = {
+		reset_packpath = false,
+		rtp = { reset = false },
+	},
+})
 
 require("trouble").setup({})
 require("todo-comments").setup({})
 require("nvim-autopairs").setup({})
--- vim.fn["firenvim#install"](0)
+-- vim.fn["firenvim--install"](0)
