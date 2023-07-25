9d2f788e7ba1aebce3b57d39c9cfbb92a1907ae9
Alper Çelik <dev.alpercelik@gmail.com>
2023-07-25 21:58:35 +0300

    Revert "try gnome"
    This reverts commit 0ccdd60dc32027f699027b94cd2a5d04ec86f052.

--- Configs/DisplayManager-os.nix
+++ Configs/DisplayManager-os.nix
@@ -1,4 +1,7 @@
 { config, lib, pkgs, ... }: {
+  # default wayland
+  services.xserver.displayManager.defaultSession = "plasmawayland";
+
   services.xserver.displayManager.gdm.wayland = true;
   services.xserver.displayManager.gdm.enable = true;
 }
--- Configs/Gnome/Gnome-os.nix
+++
@@ -1,5 +0,0 @@
-{ pkgs, ... }:
-{
-  services.xserver.desktopManager.gnome.enable = true;
-  environment.systemPackages = with pkgs.gnomeExtensions;[ ];
-}
