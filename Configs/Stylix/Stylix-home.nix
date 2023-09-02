a3e24581f29143c46d990bde89cfa221cfb9a565
Alper Çelik <dev.alpercelik@gmail.com>
2023-09-02 19:26:50 +0300

    fix kde themes not working on stylix

--- Configs/Stylix/Stylix-home.nix
+++ Configs/Stylix/Stylix-home.nix
@@ -1,5 +1,10 @@
-{ ... }:
+{ config, ... }:
 {
   imports = [ ./Stylix-generic.nix ];
   stylix.targets.vim.enable = false;
+
+  # HACK: load session variables from home manager
+  xdg.configFile."plasma-workspace/env/hm-session-vars.sh".text = ''
+    . "${config.home.profileDirectory}/etc/profile.d/hm-session-vars.sh"
+  '';
 }
