cf747d6567f73778e6d82a57a473a0b06d91beeb
Alper Çelik <dev.alpercelik@gmail.com>
2023-10-21 00:09:01 +0300

    switch to flatpak for games

--- Programs/Gui/nixos-configrations.nix
+++ Programs/Gui/nixos-configrations.nix
@@ -11,11 +11,11 @@
   programs.partition-manager.enable = true;
 
   programs.gamemode.enable = true;
-  programs.steam = {
-    enable = true;
-    remotePlay.openFirewall = true;
-    dedicatedServer.openFirewall = true;
-  };
+  # programs.steam = {
+  #   enable = true;
+  #   remotePlay.openFirewall = true;
+  #   dedicatedServer.openFirewall = true;
+  # };
 
   # Steam needs this to find Proton-GE
   environment.sessionVariables.STEAM_EXTRA_COMPAT_TOOLS_PATHS = "\${HOME}/.steam/root/compatibilitytools.d";
--- Programs/Gui/program-list.nix
+++ Programs/Gui/program-list.nix
@@ -52,7 +52,8 @@
 
 
   # games
-  heroic
+  # heroic
+  legendary-gl
   grapejuice
   wineWowPackages.stagingFull
   protonup-ng
