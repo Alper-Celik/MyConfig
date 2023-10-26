7c9de174a8af8db7222da88756033c43da7b1bd6
Alper Çelik <dev.alpercelik@gmail.com>
2023-10-26 22:01:48 +0300

    fix automatixly callpackaging of my-pkgs

--- my-pkgs/my-pkgs.nix
+++ my-pkgs/my-pkgs.nix
@@ -1,6 +1,13 @@
 { pkgs, my-lib, ... }:
 let
   package-files = my-lib.getConfigs { removed-files = [ "my-pkgs.nix" ]; base-dir = ./.; };
-  my-pkgs = builtins.foldl' (pkg-set: pkg-file: pkg-set // { ${builtins.baseNameOf "${pkg-file}"} = (pkgs.callPackage pkg-file { }); }) { } package-files;
+  my-pkgs = builtins.foldl'
+    (pkg-set: pkg-file:
+      let
+        pkg = pkgs.callPackage pkg-file { };
+      in
+      pkg-set // { "${pkg.pname}" = pkg; })
+    { }
+    package-files;
 in
-my-pkgs // { test = "adsasd"; }
+my-pkgs 
