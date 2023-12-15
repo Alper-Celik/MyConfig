0acdbd15216f236c00e4ccdd987a57c4a73c81e7
Alper Çelik <dev.alpercelik@gmail.com>
2023-12-15 17:46:07 +0300

    add clamav

---
+++ Configs/ClamAv-os.nix
@@ -0,0 +1,13 @@
+{ config, ... }:
+{
+  environment.persistence."/persistent".directories = [{
+    directory = "/var/lib/clamav";
+    user = "clamav";
+    group = "clamav";
+    # mode =
+  }];
+  services.clamav = {
+    daemon. enable = true;
+    updater.enable = true;
+  };
+}
