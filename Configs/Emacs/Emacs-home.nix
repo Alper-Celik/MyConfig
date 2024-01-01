d3aee42a9dbbcb60528fdcc6b68645bb6463b998
Alper Çelik <dev.alpercelik@gmail.com>
2024-01-02 00:54:49 +0300

    some emacs work

--- Configs/Emacs/Emacs-home.nix
+++ Configs/Emacs/Emacs-home.nix
@@ -1,6 +1,11 @@
 { inputs, pkgs, ... }:
 {
-  home.packages = with pkgs;[ emacs ];
+  programs.emacs = {
+    enable = true;
+    package = pkgs.emacs-gtk;
+    extraPackages = epkgs: [ epkgs.vterm ];
+  };
+  # home.packages = with pkgs;[ emacs-gtk ];
   home.file.".emacs.d" = {
     source = inputs.spacemacs-git;
     recursive = true;
