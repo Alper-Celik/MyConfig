a1a45a4158fc3493fd9fa8a0029cc6b807f20671
Alper Çelik <dev.alpercelik@gmail.com>
2024-01-10 16:45:30 +0300

    some work on spacemacs

--- Configs/Emacs/Emacs-home.nix
+++ Configs/Emacs/Emacs-home.nix
@@ -1,13 +1,20 @@
-{ inputs, pkgs, ... }:
-{
+{ inputs, pkgs, config, my-lib, specialArgs, ... }:
+let
+  current-dir = "Configs/Emacs";
+  outOfStrore = x:
+    config.lib.file.mkOutOfStoreSymlink
+    (my-lib.maybeOutOfStore specialArgs current-dir x);
+
+in {
   programs.emacs = {
     enable = true;
     package = pkgs.emacs-gtk;
     extraPackages = epkgs: [ epkgs.vterm ];
   };
-  # home.packages = with pkgs;[ emacs-gtk ];
+  home.packages = with pkgs; [ nixfmt ];
   home.file.".emacs.d" = {
     source = inputs.spacemacs-git;
     recursive = true;
   };
+  home.file.".spacemacs.d".source = outOfStrore ".";
 }
