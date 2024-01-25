5b1f7c2e219113d9895d8e7712ae28330a20ab2b
Alper Çelik <alper@alper-celik.dev>
2024-01-25 11:10:22 +0300

    bump flake.lock

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
   inputs =
     {
       # Nixpkgs
-      nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+      nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable-small";
       kde2nix.url = "github:nix-community/kde2nix";
 
       nixos-hardware.url = "github:nixos/nixos-hardware";
