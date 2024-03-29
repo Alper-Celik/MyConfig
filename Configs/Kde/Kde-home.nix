f3b631e31845f3db9e25273781b5b3453f65a316
Alper Çelik <alper@alper-celik.dev>
2024-03-29 22:08:48 +0300

    update shortcut on kde

--- Configs/Kde/Kde-home.nix
+++ Configs/Kde/Kde-home.nix
@@ -4,10 +4,12 @@
 
     workspace.clickItemTo = "select";
 
-    shortcuts = {
-      "kitty.desktop"._launch = "Ctrl+Alt+T";
-
+    hotkeys.commands."launch-kitty" = {
+      name = "Launch Kitty";
+      key = "Ctrl+Alt+T";
+      command = "kitty";
     };
+
     configFile = {
       "baloofilerc"."Basic Settings"."Indexing-Enabled".value = false; #disable indexing
       "kxkbrc"."Layout"."Options".value = "caps:escape_shifted_capslock"; # capslock to esc
