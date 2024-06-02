1664546cca7ba41a565f8fc8db6e8eb9ff68ec03
Alper Çelik <alper@alper-celik.dev>
2024-06-03 00:45:03 +0300

    add memtest

--- Configs/Boot/Boot-os-lenovo-ideapad-510.nix
+++ Configs/Boot/Boot-os-lenovo-ideapad-510.nix
@@ -1,5 +1,6 @@
 { pkgs, ... }:
 {
+  boot.loader.systemd-boot.memtest86.enable = true;
   boot.loader.systemd-boot.enable = true;
   boot.loader.timeout = 0;
   boot.loader.efi.canTouchEfiVariables = true;
