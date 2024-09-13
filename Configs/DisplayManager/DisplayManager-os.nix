d6905c8125002b48a3c58ff7cd9d00c8aae3e9f9
Alper Çelik <alper@alper-celik.dev>
2024-09-14 00:48:40 +0300

    make sddm a bit better

--- Configs/DisplayManager/DisplayManager-os.nix
+++ Configs/DisplayManager/DisplayManager-os.nix
@@ -7,6 +7,12 @@
 {
   # services.xserver.displayManager.gdm.wayland = true;
   # services.xserver.displayManager.gdm.enable = true;
-  services.displayManager.sddm.enable = true;
-  services.displayManager.sddm.wayland.enable = true;
+  services.displayManager.sddm = {
+    enable = true;
+    wayland = {
+      enable = true;
+      compositor = "kwin";
+    };
+    autoNumlock = true;
+  };
 }
