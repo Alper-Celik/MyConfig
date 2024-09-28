9b90e6693e16f8ac9b1c3b02487ad586a27e5278
Alper Çelik <alper@alper-celik.dev>
2024-09-29 00:49:05 +0300

    add gamescope

--- Configs/Games-os.nix
+++ Configs/Games-os.nix
@@ -1,4 +1,12 @@
 { pkgs, ... }:
+let
+  gamescope-env = {
+    __NV_PRIME_RENDER_OFFLOAD = "1";
+    __VK_LAYER_NV_optimus = "NVIDIA_only";
+    __GLX_VENDOR_LIBRARY_NAME = "nvidia";
+  };
+
+in
 {
   environment.systemPackages = with pkgs; [
     heroic
@@ -9,11 +17,24 @@
 
     wineWowPackages.stagingFull
     protonup-qt
+    steamtinkerlaunch
 
   ];
 
   programs.steam = {
     enable = true;
     remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    localNetworkGameTransfers.openFirewall = true;
+    gamescopeSession = {
+      enable = true;
+      env = gamescope-env;
+      args = [ "--rt" ];
+    };
+  };
+  programs.gamescope = {
+    enable = true;
+    env = gamescope-env;
+    capSysNice = true;
+    args = [ "--rt" ];
   };
 }
