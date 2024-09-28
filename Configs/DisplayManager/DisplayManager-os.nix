10c69bf08bc898e7d5941f36e2ba8969c47fa75b
Alper Çelik <alper@alper-celik.dev>
2024-09-28 18:55:45 +0300

    use sddm

--- Configs/DisplayManager/DisplayManager-os.nix
+++ Configs/DisplayManager/DisplayManager-os.nix
@@ -6,25 +6,25 @@
 }:
 {
   services.displayManager.defaultSession = "plasma";
-  programs.dconf.profiles.gdm.databases = [
-    {
-      settings."org/gnome/desktop/peripherals/keyboard" = {
-        numlock-state = true;
-        remember-numlock-state = true;
-      };
-    }
-  ];
-  services.xserver.displayManager.gdm = {
-    wayland = true;
-    enable = true;
-  };
-
-  # services.displayManager.sddm = {
+  # programs.dconf.profiles.gdm.databases = [
+  #   {
+  #     settings."org/gnome/desktop/peripherals/keyboard" = {
+  #       numlock-state = true;
+  #       remember-numlock-state = true;
+  #     };
+  #   }
+  # ];
+  # services.xserver.displayManager.gdm = {
+  #   wayland = true;
   #   enable = true;
-  #   wayland = {
-  #     enable = true;
-  #     compositor = "kwin";
-  #   };
-  #   autoNumlock = true;
   # };
+
+  services.displayManager.sddm = {
+    enable = true;
+    wayland = {
+      enable = true;
+      compositor = "kwin";
+    };
+    autoNumlock = true;
+  };
 }
