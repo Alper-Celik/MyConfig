bf0e65cf35aa8e1f076712001b78b5dc3335b1da
Alper Çelik <alper@alper-celik.dev>
2024-10-08 11:59:50 +0300

    more tailscale

--- Configs/Tailscale-os.nix
+++ Configs/Tailscale-os.nix
@@ -1,6 +1,22 @@
-{ ... }:
+{ pkgs, ... }:
 {
-  services.tailscale.enable = true;
+  environment.systemPackages = [
+    pkgs.ktailctl
+  ];
+
+  systemd.user.services.ktailscale = {
+    enable = true;
+    description = "autostart ktailctl";
+    wantedBy = [ "default.target" ];
+    serviceConfig = {
+      ExecStart = "${pkgs.ktailctl}/bin/ktailctl";
+    };
+  };
+
+  services.tailscale = {
+    enable = true;
+    useRoutingFeatures = "client";
+  };
 
   networking.nameservers = [
     "100.100.100.100"
