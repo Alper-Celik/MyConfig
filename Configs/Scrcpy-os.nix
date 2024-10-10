e4d2e74e40539db218d7203f7c550c68be1aa45a
Alper Çelik <alper@alper-celik.dev>
2024-10-10 17:19:20 +0300

    some v4l2loopback options

--- Configs/Scrcpy-os.nix
+++ Configs/Scrcpy-os.nix
@@ -8,6 +8,7 @@
   boot = {
     extraModulePackages = [ config.boot.kernelPackages.v4l2loopback ];
     kernelModules = [ "v4l2loopback" ];
+    extraModprobeConfig = "options v4l2loopback exclusive_caps=1 devices=3";
   };
   users.users.alper.extraGroups = [ "adbusers" ];
 }
