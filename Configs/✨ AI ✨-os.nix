f15ff7c1b5e1269961cfb65ceb1fed72e6433206
Alper Çelik <alper@alper-celik.dev>
2024-10-19 03:13:14 +0300

    switch to using openwebui using docker to use less bandwidth and storage

--- Configs/✨ AI ✨-os.nix
+++ Configs/✨ AI ✨-os.nix
@@ -1,13 +1,15 @@
-{ ... }:
+{ config, ... }:
 {
   services.ollama = {
     enable = true;
   };
 
-  services.open-webui = {
-    enable = true;
+  virtualisation.oci-containers.containers."open-webui" = {
+    image = "ghcr.io/open-webui/open-webui:0.3.32";
+    volumes = [ "open-webui:/app/backend/data" ];
+    ports = [ "8080:8080" ];
+    extraOptions = [ "--network=host" ];
     environment = {
-      # Disable authentication
       WEBUI_AUTH = "False";
     };
   };
