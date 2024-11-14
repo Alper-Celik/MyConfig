d1e95c947dc5b6db78ec7faf08919123cd45b23d
Alper Çelik <alper@alper-celik.dev>
2024-11-15 01:55:21 +0300

    .

--- Configs/ssh.home.nix
+++ Configs/ssh.home.nix
@@ -3,6 +3,9 @@
   programs.ssh = {
     enable = true;
     matchBlocks = {
+      "rpi5.devices.alper-celik.dev" = {
+        port = 110;
+      };
       "github.com" = {
         hostname = "ssh.github.com";
         port = 443;
