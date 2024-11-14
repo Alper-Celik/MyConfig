f62abbc45b5aeaee62e0901c55028822bf08965c
Alper Çelik <alper@alper-celik.dev>
2024-11-14 15:38:35 +0300

    use https port to cicumvent port 22 block on our universitys eduroam network

---
+++ Configs/ssh.home.nix
@@ -0,0 +1,14 @@
+{ ... }:
+{
+  programs.ssh = {
+    enable = true;
+    matchBlocks = {
+      "github.com" = {
+        hostname = "ssh.github.com";
+        port = 443;
+        user = "git";
+      };
+    };
+  };
+
+}
