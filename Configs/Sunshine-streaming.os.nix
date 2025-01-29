436b16ae04a0abdbdfc5ff5fc20de2be1ad9b6f5
Alper Çelik <alper@alper-celik.dev>
2025-01-29 20:25:39 +0100

    bump flake.lock

--- Configs/Sunshine-streaming.os.nix
+++ Configs/Sunshine-streaming.os.nix
@@ -1,7 +1,7 @@
 { ... }:
 {
   services.sunshine = {
-    enable = true;
+    # enable = true;
     autoStart = true;
     capSysAdmin = true; # only needed for Wayland -- omit this when using with Xorg
     openFirewall = true;
