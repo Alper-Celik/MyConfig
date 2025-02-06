34befb1c8cc282ec060a647b41ab73c585fe1036
Alper Çelik <alper@alper-celik.dev>
2025-02-07 02:59:37 +0300

    enable sunshine

--- Configs/Sunshine-streaming.os.nix
+++ Configs/Sunshine-streaming.os.nix
@@ -1,7 +1,12 @@
-{ ... }:
+{
+  pkgs,
+  pkgs-unstable,
+  ...
+}:
 {
   services.sunshine = {
-    # enable = true;
+    package = pkgs-unstable.sunshine;
+    enable = true;
     autoStart = true;
     capSysAdmin = true; # only needed for Wayland -- omit this when using with Xorg
     openFirewall = true;
