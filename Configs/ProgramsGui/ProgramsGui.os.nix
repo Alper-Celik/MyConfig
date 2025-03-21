62436f28d72d1aefbfff37f483bfa33c39892b86
Alper Çelik <alper@alper-celik.dev>
2025-03-21 15:42:03 +0300

    switch to only office

--- Configs/ProgramsGui/ProgramsGui.os.nix
+++ Configs/ProgramsGui/ProgramsGui.os.nix
@@ -43,7 +43,7 @@
       # neochat
       ## office suits
       # onlyoffice-desktopeditors
-      libreoffice
+      onlyoffice-desktopeditors
       ## file mangement
       ark
       onedrivegui
