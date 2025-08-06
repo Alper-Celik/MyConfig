26b369dc174ed8aa683df016a55e8f0e44031ab7
Alper Çelik <alper@alper-celik.dev>
2025-08-06 17:50:47 +0300

    finish dotnet config

--- Configs/Neovim/lua/plugins/dotnet-with-rosyln.lua
+++ Configs/Neovim/lua/plugins/dotnet-with-rosyln.lua
@@ -1,8 +1,8 @@
 return {
   {
-
     "seblyng/roslyn.nvim",
     ft = { "cs", "razor" },
+    lazy = false,
     dependencies = {
       {
         -- By loading as a dependencies, we ensure that we are available to set
@@ -11,7 +11,8 @@ return {
         config = true,
       },
     },
-    config = function()
+    config = function(_, opts)
+      require("roslyn").setup(opts)
       local rzls_base_path = vim.env.RZLS_ROOT_DIR
       vim.lsp.config("roslyn", {
         cmd = {
@@ -24,6 +25,9 @@ return {
             .. vim.fs.joinpath(rzls_base_path, "Targets", "Microsoft.NET.Sdk.Razor.DesignTime.targets"),
         },
         settings = {
+          ["csharp|code_lens"] = {
+            dotnet_enable_references_code_lens = true,
+          },
           ["csharp|inlay_hints"] = {
             csharp_enable_inlay_hints_for_implicit_object_creation = true,
             csharp_enable_inlay_hints_for_implicit_variable_types = true,
@@ -42,5 +46,59 @@ return {
         },
       })
     end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = { ensure_installed = { "c_sharp" } },
+  },
+  {
+    "Issafalcon/neotest-dotnet",
+  },
+  {
+    "mfussenegger/nvim-dap",
+    opts = function()
+      local dap = require("dap")
+      if not dap.adapters["netcoredbg"] then
+        require("dap").adapters["netcoredbg"] = {
+          type = "executable",
+          command = vim.fn.exepath("netcoredbg"),
+          args = { "--interpreter=vscode" },
+          options = {
+            detached = false,
+          },
+        }
+      end
+      for _, lang in ipairs({ "cs", "fsharp", "vb" }) do
+        if not dap.configurations[lang] then
+          dap.configurations[lang] = {
+            {
+              type = "netcoredbg",
+              name = "Launch file",
+              request = "launch",
+              ---@diagnostic disable-next-line: redundant-parameter
+              program = function()
+                return vim.fn.input("Path to dll: ", vim.fn.getcwd() .. "/", "file")
+              end,
+              cwd = "${workspaceFolder}",
+            },
+          }
+        end
+      end
+    end,
+  },
+
+  {
+    "nvim-neotest/neotest",
+    optional = true,
+    dependencies = {
+      "Issafalcon/neotest-dotnet",
+    },
+    opts = {
+      adapters = {
+        ["neotest-dotnet"] = {
+          -- Here we can set options for neotest-dotnet
+        },
+      },
+    },
   },
 }
