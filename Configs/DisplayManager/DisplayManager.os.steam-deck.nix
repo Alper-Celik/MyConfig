cd59ee60232ecd72ef1ea2e8232a073c14adcea8
Alper Çelik <alper@alper-celik.dev>
2025-10-06 19:19:04 +0300

    Revert "add initial steam deck support"
    This reverts commit 996356e6ae43a14c1d8b2d262520db13eaca0674.

--- Configs/DisplayManager/DisplayManager.os.steam-deck.nix
+++
@@ -1,9 +0,0 @@
-{ lib, config, ... }:
-{
-  services.displayManager.sddm.enable = lib.mkForce false;
-  jovian.steam = {
-    desktopSession = config.services.displayManager.defaultSession;
-    autoStart = true;
-    user = "alper";
-  };
-}
--- Configs/Games.os.nix
+++ Configs/Games.os.nix
@@ -18,6 +18,7 @@ in
 
     protonup-qt
     steamtinkerlaunch
+    gamescope
   ];
 
   programs.steam = {
