c5d6d90a4c6ec5d11133f511b83cb9ad7666a61e
Alper Çelik <alper@alper-celik.dev>
2026-01-11 20:23:04 +0300

    some cshaprp nvim refactor

--- Configs/Neovim/lua/plugins/dotnet-with-rosyln.lua
+++ Configs/Neovim/lua/plugins/dotnet-with-rosyln.lua
@@ -3,27 +3,19 @@ return {
     "seblyng/roslyn.nvim",
     ft = { "cs", "razor" },
     lazy = false,
-    dependencies = {
-      {
-        -- By loading as a dependencies, we ensure that we are available to set
-        -- the handlers for Roslyn.
-        "tris203/rzls.nvim",
-        config = true,
-      },
-    },
     config = function(_, opts)
       require("roslyn").setup(opts)
-      local rzls_base_path = vim.env.RZLS_ROOT_DIR
+      -- local rzls_base_path = vim.env.RZLS_ROOT_DIR
       vim.lsp.config("roslyn", {
-        cmd = {
-          "Microsoft.CodeAnalysis.LanguageServer",
-          "--logLevel=Information",
-          "--extensionLogDirectory=" .. vim.fs.dirname(vim.lsp.get_log_path()),
-          "--stdio",
-          "--razorSourceGenerator=" .. vim.fs.joinpath(rzls_base_path, "Microsoft.CodeAnalysis.Razor.Compiler.dll"),
-          "--razorDesignTimePath="
-            .. vim.fs.joinpath(rzls_base_path, "Targets", "Microsoft.NET.Sdk.Razor.DesignTime.targets"),
-        },
+        -- cmd = {
+        --   "Microsoft.CodeAnalysis.LanguageServer",
+        --   "--logLevel=Information",
+        --   "--extensionLogDirectory=" .. vim.fs.dirname(vim.lsp.get_log_path()),
+        --   "--stdio",
+        --   "--razorSourceGenerator=" .. vim.fs.joinpath(rzls_base_path, "Microsoft.CodeAnalysis.Razor.Compiler.dll"),
+        --   "--razorDesignTimePath="
+        --     .. vim.fs.joinpath(rzls_base_path, "Targets", "Microsoft.NET.Sdk.Razor.DesignTime.targets"),
+        -- },
         settings = {
           ["csharp|code_lens"] = {
             dotnet_enable_references_code_lens = true,
