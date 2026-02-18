87d45c8fe59dbcb60471c7cd58350c31e1832d93
Alper Çelik <alper@alper-celik.dev>
2026-02-18 22:36:23 +0300

    add sunshine with nvencc encoding

--- Configs/Sunshine.os.nix
+++ Configs/Sunshine.os.nix
@@ -1,9 +1,14 @@
-{ ... }:
+{ pkgs, ... }:
 {
   services.sunshine = {
     enable = true;
     autoStart = true;
     capSysAdmin = true;
     openFirewall = true;
+    package = pkgs.sunshine.override { cudaSupport = true; };
   };
+  users.users.alper.extraGroups = [
+    "video"
+    "render"
+  ];
 }
