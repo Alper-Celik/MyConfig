d469d4eefc97b2d89ab290e4458fa6c385fc917a
Alper Çelik <alper@alper-celik.dev>
2026-03-10 01:55:30 +0300

    ai things

--- Configs/✨ AI ✨.os.nix
+++ Configs/✨ AI ✨.os.nix
@@ -9,6 +9,32 @@
   environment.systemPackages = with pkgs; [
     opencode
   ];
+  environment.sessionVariables = {
+    OPENCODE_ENABLE_EXA = 1; # enable web search
+    OPENCODE_CONFIG = pkgs.writeText "opencode-config" (
+      builtins.toJSON {
+        "$schema" = "https://opencode.ai/config.json";
+        permission = {
+          bash = "ask";
+        };
+
+        provider = {
+          "ollama" = {
+            "npm" = "@ai-sdk/openai-compatible";
+            "name" = "Ollama";
+            "options" = {
+              "baseURL" = "http://localhost:${builtins.toString config.services.ollama.port}/v1";
+            };
+            "models" = {
+              "qwen3.5=9b" = {
+                "name" = "qwen3.5:9b";
+              };
+            };
+          };
+        };
+      }
+    );
+  };
 
   environment.persistence."/persistent".directories = [
     {
@@ -19,7 +45,7 @@
     }
   ];
 
-  # FIXME: systemd creates problems fix in near future
+  # FIXME= systemd creates problems fix in near future
   services.ollama = rec {
     package = pkgs-unstable.ollama-cuda;
     enable = true;
