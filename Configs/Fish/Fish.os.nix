05d16c9d856c2a4916cea53757e038f21bf384cc
Alper Çelik <alper@alper-celik.dev>
2026-03-10 01:56:17 +0300

    switch to starship from tide

--- Configs/Fish/Fish.os.nix
+++ Configs/Fish/Fish.os.nix
@@ -4,10 +4,16 @@
   environment.systemPackages = with pkgs; [
     fish
     any-nix-shell
+    starship
   ];
   programs.fish.enable = true;
   programs.fish.promptInit = ''
     any-nix-shell fish --info-right | source
   '';
   users.users.alper.shell = pkgs.fish;
+
+  programs.starship = {
+    enable = true;
+    presets = [ "nerd-font-symbols" ];
+  };
 }
