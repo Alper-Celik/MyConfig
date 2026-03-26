10dfe79d682c7c4d1137101f63f2cdc9d4ea0d9d
Alper Çelik <alper@alper-celik.dev>
2026-03-26 13:05:59 +0300

    Revert "switch back to nix from lix since lix doesnt support shebang"
    This reverts commit 1548a76ba52c7a5703894f45b3a2cf69ab68dedd.
    
    when i think about it when it is needed i can just include in projects
    nix shell to have the upstream nix

--- Configs/Nix.os.nix
+++ Configs/Nix.os.nix
@@ -50,4 +50,17 @@
     Nice = 10;
   };
 
+  #use lix
+  nixpkgs.overlays = [
+    (final: prev: {
+      inherit (prev.lixPackageSets.stable)
+        nixpkgs-review
+        nix-eval-jobs
+        nix-fast-build
+        colmena
+        ;
+    })
+  ];
+  nix.package = pkgs.lixPackageSets.stable.lix;
+
 }
