78e3848f7340bf15f3cf4e7045878382b97fbe1e
Alper Çelik <alper@alper-celik.dev>
2026-07-12 12:02:42 +0300

    bump flake.lock

--- Configs/Hardware/Hardware.os.macbook-m1-alper.nix
+++ Configs/Hardware/Hardware.os.macbook-m1-alper.nix
@@ -20,6 +20,7 @@
   hardware.bluetooth.enable = true;
 
   networking.networkmanager.wifi.backend = "iwd";
+  hardware.asahi.enable = true;
   hardware.asahi.peripheralFirmwareDirectory = inputs.macbook-m1-alper-firmware;
   boot.extraModprobeConfig = ''
     options hid_apple swap_opt_cmd=1
