7cf7e37e3815ac8e9941c9cad747572e7986c55e
Alper Çelik <alper@alper-celik.dev>
2026-08-16 15:31:19 +0300

    fix tailscale mulvad problem

--- Configs/Networking.os.nix
+++ Configs/Networking.os.nix
@@ -24,6 +24,7 @@
       chain excludeOutgoing {
         type route hook output priority 0; policy accept;
         ip daddr 100.64.0.0/10 ct mark set 0x00000f41 meta mark set 0x6d6f6c65;
+        ip saddr 100.64.0.0/10 ct mark set 0x00000f41 meta mark set 0x6d6f6c65;
       }
     '';
   };
