7f85b1a26dfb056c0a4ba6dfcab5872e945db9a3
Alper Çelik <alper@alper-celik.dev>
2026-08-21 19:17:10 +0300

    switch back to viciane

--- Configs/Vicinae/Vicinae.home.nix
+++ Configs/Vicinae/Vicinae.home.nix
@@ -2,28 +2,52 @@
   inputs,
   pkgs,
   lib,
+  config,
+  my-lib,
+  specialArgs,
   ...
 }:
+let
+  current-dir = "Configs/Vicinae";
+  outOfStrore =
+    x: config.lib.file.mkOutOfStoreSymlink (my-lib.maybeOutOfStore specialArgs current-dir x);
+in
 {
   programs.vicinae = {
     #package = inputs.vicinae.packages.${pkgs.stdenv.hostPlatform.system}.default;
+    enable = true;
     useLayerShell = true;
     enableFirefoxIntegration = true;
     systemd = {
       enable = true;
     };
-
-    settings = {
-
-      launcher_window = {
-        opacity = 0.8;
-      };
-    };
-
+    settings = lib.mkForce { };
     extensions = with inputs.vicinae-extensions.packages.${pkgs.stdenv.hostPlatform.system}; [
       nix
       power-profile
+      process-manager
+      # bluetooth
+      wifi-commander
+      # systemd
+      firefox
+      niri
+      player-pilot
+      port-killer
+      github
+      flathub-search
+      # dbus
+      color-converter
+      nerdfont-search
+      wikipedia
+      mullvad
+      zoxide-recent-directories
+      kaomojis
+      niri-monitors
+      dms-integration
+      lorem-ipsum
     ];
   };
+
+  xdg.configFile."vicinae".source = outOfStrore ".";
 
 }
